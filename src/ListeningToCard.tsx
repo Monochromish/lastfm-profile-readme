f3419a4618074b671adc85b7b1b9221a43375896
Monochromish <chillygamer7@gmail.com>
2023-03-01 18:09:14 +1100

    TypeScript practices + URL Queries

--- src/ListeningToCard.tsx
+++ src/ListeningToCard.tsx
@@ -1,10 +1,19 @@
 import https from 'https';
 
+interface ListeningToCardProps {
+	color?: string;
+	textColor?: string;
+	isRounded?: boolean;
+	displayName?: boolean;
+}
+
 const ListeningToCard = async (
+	username: string,
 	name: string,
 	artistName: string,
 	albumName: string,
 	artwork: string,
+	props?: ListeningToCardProps,
 ) => {
 	const artworkBase64 = await new Promise<string>((resolve, reject) => {
 		https.get(artwork, res => {
@@ -23,12 +32,14 @@ const ListeningToCard = async (
 
 	return `
     <svg viewBox="0 0 400 100" xmlns="http://www.w3.org/2000/svg">
-      <rect width="400" height="100" fill="#070D0D" rx="8" />
+      <rect width="400" height="100" fill="#${props.color}" rx="${props.isRounded ? 8 : 0}" />
       <clipPath id="artwork-clip">
         <rect x="8" y="8" width="80" height="84" rx="16" />
       </clipPath>
       <image href="data:image/jpeg;base64,${artworkBase64}" x="16" y="15" width="64" height="64" clipPath="url(#artwork-clip)" />
-      <text font-family="system-ui" x="100" y="35" fill="#666666" className="text-sm font-medium" dominantBaseline="middle">
+      <text font-family="system-ui" x="100" y="35" fill="${
+				props.textColor
+			}" className="text-sm font-medium" dominantBaseline="middle">
         Currently Listening To
       </text>
       <text font-family="system-ui" x="100" y="55" fill="#F9FAFB" className="text-lg font-bold" dominantBaseline="middle">
@@ -37,6 +48,15 @@ const ListeningToCard = async (
       <text font-family="system-ui" x="100" y="70" fill="#F9FAFB" className="text-sm font-medium" dominantBaseline="middle">
         By ${encodeText(artistName)} on ${encodeText(albumName)}
       </text>
+	   ${
+				props.displayName
+					? `<text font-family="system-ui" x="395" y="95" fill="${
+							props.textColor
+					  }" className="text-sm font-medium" dominantBaseline="baseline" text-anchor="end">${
+							username || ''
+					  }</text>`
+					: ''
+			}
     </svg>
   `;
 };
--- src/pages/api/[username].tsx
+++ src/pages/api/[username].tsx
@@ -1,45 +1,91 @@
-import ListeningToCard from '../../ListeningToCard';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import ListeningToCard from '../../ListeningToCard';
 
-type Data = {
-	id?: string | string[];
-	error?: any;
-	code?: string;
-};
+interface LastFMResponse {
+	recenttracks: {
+		track: {
+			name: string;
+			artist: {
+				'#text': string;
+			};
+			album: {
+				'#text': string;
+			};
+			image: {
+				'#text': string;
+			}[];
+		}[];
+	};
+}
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-	let lastFMState;
-	const apiKey = process.env.NEXT_PUBLIC_LASTFM_API?.toString(),
-		username = req.query.username as string;
+interface ErrorResponse {
+	error: string;
+}
+
+interface SuccessResponse {
+	svg: string;
+}
+
+type Response = ErrorResponse | SuccessResponse;
+
+interface QueryParams {
+	color?: string;
+	textColor?: string;
+	isRounded?: boolean;
+	displayName?: boolean;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
+	const apiKey = process.env.NEXT_PUBLIC_LASTFM_API;
+	const username = req.query.username as string;
+	const queryParams = req.query as QueryParams;
+
+	const {
+		color = '#000',
+		textColor = '#999',
+		isRounded = false,
+		displayName = false,
+	} = queryParams;
 
 	try {
-		lastFMState = await axios(
+		const { data } = await axios.get<LastFMResponse>(
 			`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${username}&api_key=${apiKey}&format=json&limit=1`,
 		);
-	} catch (err) {
-		res.status(400).send({
-			error: `If it is taking time, make sure last.fm account under the username ${req.query.username} exists and is scrobbling`,
-		});
+
+		const track = data.recenttracks.track[0];
+
+		if (!track) {
+			throw new Error(
+				`Something went wrong. Make sure last.fm account under the username ${username} exists`,
+			);
+		}
+
+		res.setHeader('Content-Type', 'image/svg+xml; charset=utf-8');
+		res.setHeader(
+			'content-security-policy',
+			"default-src 'none'; img-src * data:; style-src 'unsafe-inline'",
+		);
+
+		const svg = await ListeningToCard(
+			username,
+			track.name,
+			track.artist['#text'],
+			track.album['#text'],
+			track.image[3]['#text'],
+			{
+				color,
+				textColor,
+				isRounded,
+				displayName,
+			},
+		);
+
+		res.send(svg as any);
+	} catch (error) {
+		const message =
+			error.message ||
+			`Something went wrong. Make sure last.fm account under the username ${username} exists`;
+		res.status(400).send({ error: message });
 	}
-
-	if (!lastFMState.data.recenttracks.track[0])
-		res.status(400).send({
-			error: `If it is taking time, make sure last.fm account under the username ${req.query.username} exists and is scrobbling`,
-		});
-
-	res.setHeader('Content-Type', 'image/svg+xml; charset=utf-8');
-	res.setHeader(
-		'content-security-policy',
-		"default-src 'none'; img-src * data:; style-src 'unsafe-inline'",
-	);
-
-	const svg = await ListeningToCard(
-		lastFMState.data.recenttracks.track[0].name,
-		lastFMState.data.recenttracks.track[0].artist['#text'],
-		lastFMState.data.recenttracks.track[0].album['#text'],
-		lastFMState.data.recenttracks.track[0].image[3]['#text'],
-	);
-
-	res.send(svg as any);
 }
